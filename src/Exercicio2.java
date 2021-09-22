08220fffc24b2baec3f5453c8f23c4c619843ff8
Bruno Grassi Ferreira <brunoferreira03@globo.com>
2021-09-22 09:39:11 -0300

    Parte 2

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -6,6 +6,7 @@ public class Exercicio2 {
     public static void main(String[] args) {
         // Criar um arrayList de Strings e inicializá-lo com uma nova lista vazia
         ArrayList<String> listaDeAnimais = null;
+        listaDeAnimais = new ArrayList<>();
 
         // Inicializando com valores
         // List<String> listaDeAnimais = Arrays.asList("Pato", "Cachorro", "Gato");
@@ -13,17 +14,17 @@ public class Exercicio2 {
 
         // Bloco Try-Catch para proteger bloco de possível Exception
         // Criar uma cláusula try-catch para controlar a exceção anterior
-        try {
-
         // Adicionar os seguintes valores: "Pato", "Cachorro" e "Gato"
         listaDeAnimais.add("Pato"); // index: 0
         listaDeAnimais.add("Cachorro"); // index: 1
         listaDeAnimais.add("Gato"); // index: 2
 
-        // Imprimir o conteúdo da Posição 2
-            System.out.println("Animal da posição 2: " + listaDeAnimais.get(2));
-        } catch (NullPointerException e) {
-            System.err.println("NullPointerException: " + e.getMessage());
+        try {
+
+        // Imprimir o conteúdo da Posição 5
+            System.out.println("Animal da posição 5: " + listaDeAnimais.get(5));
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("IndexOutOfBoundsException: " + e.getMessage());
         }
         // Executar o programa. O que acontece?
     }
